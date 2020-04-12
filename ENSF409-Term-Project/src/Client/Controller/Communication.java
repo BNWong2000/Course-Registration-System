23e9a5036a90989b208d919627a1ca3070f519a4
Savi Jessel <savipal.jessel@gmail.com>
2020-04-12 15:50:46 -0600

    communication updated

--- ENSF409-Term-Project/src/Client/Controller/Communication.java
+++ ENSF409-Term-Project/src/Client/Controller/Communication.java
@@ -1,5 +1,47 @@
 package Client.Controller;
 
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Communication {
-    //
+
+    private Socket aSocket;
+
+    private ObjectInputStream socketIn;
+
+    private ObjectOutputStream socketOut;
+
+
+    public Communication(String serverName, int portNumber) {
+        try {
+            aSocket = new Socket(serverName, portNumber);
+            socketIn = new ObjectInputStream(aSocket.getInputStream());
+            socketOut = new ObjectOutputStream((aSocket.getOutputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Object communicate(Object object, String label){
+
+        Object incomingObject = null;
+
+        try{
+            socketOut.writeObject(object);
+            socketOut.writeObject(label);
+            socketOut.flush();
+            incomingObject = socketIn.readObject();
+
+        } catch (IOException e) {
+            // do something else
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return incomingObject;
+    }
 }
