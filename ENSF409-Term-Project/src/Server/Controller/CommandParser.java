c6a5f5a8657ed1de6fa6d23516787980e91d8ab3
BNWong2000 <brandenw2000@gmail.com>
2020-04-12 17:51:21 -0600

    started command parser

--- ENSF409-Term-Project/src/Server/Controller/CommandParser.java
+++ ENSF409-Term-Project/src/Server/Controller/CommandParser.java
@@ -1,4 +1,25 @@
 package Server.Controller;
 
 public class CommandParser {
+    private int numArguments;
+
+    public int parseCommand(String commandNum){
+        switch(commandNum){
+            case "1":
+                numArguments = 1;
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            default:
+                //invalid input
+                return -1;
+        }
+        return Integer.parseInt(commandNum);
+    }
 }
