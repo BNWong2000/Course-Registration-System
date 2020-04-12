e772fe775f5a92a702318015870489899aba990d
Savi Jessel <savipal.jessel@gmail.com>
2020-04-12 16:40:44 -0600

    search controller

--- ENSF409-Term-Project/src/Client/Controller/SearchController.java
+++ ENSF409-Term-Project/src/Client/Controller/SearchController.java
@@ -2,12 +2,47 @@ package Client.Controller;
 
 import Client.View.SearchView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class SearchController {
 
     private SearchView searchView;
 
     SearchController(SearchView searchView){
         this.searchView = searchView;
+    }
+
+    public class SearchListener implements ActionListener {
+        @Override
+
+        public void actionPerformed(ActionEvent e) {
+            // List to contain the id, faculty, major, and year
+            ArrayList<String> fields = new ArrayList<String>(searchView.getFields());
+            // check if any of the fields are empty
+            for (String s : fields) {
+                if (s.equals("")) {
+                    searchView.sendDialogueMessage("One or more of the fields is empty");
+                    return;
+                }
+            }
+
+            
+
+
+        }
+        //
+    }
+
+    public class CancelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            searchView.clearTextFields();
+            searchView.setVisible(false);
+
+        }
+        //
     }
 
 
