456ee22241bca98df49b2fb2a95fee9b32106641
Savi Jessel <savipal.jessel@gmail.com>
2020-04-12 17:43:26 -0600

    server communications

--- ENSF409-Term-Project/src/Server/Controller/Communication.java
+++ ENSF409-Term-Project/src/Server/Controller/Communication.java
@@ -5,6 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executors;
 
 public class Communication {
 
@@ -18,7 +19,16 @@ public class Communication {
 
     private CommandParser parser;
 
-    public Communication(){
+    public Communication(int port){
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("The server is now running...");
+            socketIn = new ObjectInputStream(aSocket.getInputStream());
+            socketOut = new ObjectOutputStream((aSocket.getOutputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
