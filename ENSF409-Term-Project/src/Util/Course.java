c1f2c97b36f52af81da45998759bc1156f3a5786
Branden Wong <branden.wong1@ucalgary.ca>
2020-04-13 17:23:32 -0600

    Finished server side javadocs

--- ENSF409-Term-Project/src/Util/Course.java
+++ ENSF409-Term-Project/src/Util/Course.java
@@ -5,16 +5,43 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
+ * A class to store information about courses
  * @author Branden Wong - 30040675
  * @author Savipal Jessel - 30039257
+ * @version 2.0
  */
 public class Course implements Serializable {
+
+	/**
+	 * Serial ID for this class
+	 */
 	private static final long serialVersionUID=1L;
+
+	/**
+	 * the name of the course
+	 */
 	private String courseName;
+
+	/**
+	 * the course number
+	 */
 	private int courseNum;
+
+	/**
+	 * A list of prerequisite courses
+	 */
 	private ArrayList<Course> preReq;
+
+	/**
+	 * A list of offerings for the course
+	 */
 	private ArrayList<CourseOffering> offeringList;
 
+	/**
+	 * A constructor for the course. Assigns the name and number
+	 * @param courseName the name of the course
+	 * @param courseNum the number of the course
+	 */
 	public Course(String courseName, int courseNum) {
 		this.setCourseName(courseName);
 		this.setCourseNum(courseNum);
@@ -23,6 +50,10 @@ public class Course implements Serializable {
 		offeringList = new ArrayList<CourseOffering>();
 	}
 
+	/**
+	 * Adds an offering to the offering list
+	 * @param offering the offering to add
+	 */
 	public void addOffering(CourseOffering offering) {
 		if (offering != null && offering.getTheCourse() == null) {
 			offering.setTheCourse(this);
@@ -36,29 +67,58 @@ public class Course implements Serializable {
 		}
 	}
 
+	/**
+	 * Gets the name of the course
+	 * @return the name of the course
+	 */
 	public String getCourseName() {
 		return courseName;
 	}
 
+	/**
+	 * Sets the name of the course
+	 * @param courseName the name to set the course to.
+	 */
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
 
+	/**
+	 * Gets the course number
+	 * @return the course number
+	 */
 	public int getCourseNum() {
 		return courseNum;
 	}
 
+	/**
+	 * Sets the course number
+	 * @param courseNum the number to set the course number to.
+	 */
 	public void setCourseNum(int courseNum) {
 		this.courseNum = courseNum;
 	}
 
+	/**
+	 * Adds a prerequisite to the pre-req. list.
+	 * @param preReq the prerequisite course to add.
+	 */
 	public void addPreReq(Course preReq){
 		this.preReq.add(preReq);
 	}
 
+	/**
+	 * Gets the number of offerings for the course.
+	 * @return the number of offerings for the course.
+	 */
 	public int getNumOfferings(){
 		return offeringList.size();
 	}
+
+	/**
+	 * Returns the string format of the course, for printing
+	 * @return the string format of the course information.
+	 */
 	@Override
 	public String toString () {
 		String st = "\n";
@@ -70,6 +130,11 @@ public class Course implements Serializable {
 		return st;
 	}
 
+	/**
+	 * Gets a specific course offering at a given index.
+	 * @param i the index to get the offering from
+	 * @return the course offering.
+	 */
 	public CourseOffering getCourseOfferingAt(int i) {
 		// TODO Auto-generated method stub
 		if (i < 0 || i >= offeringList.size() )
--- ENSF409-Term-Project/src/Util/Student.java
+++ ENSF409-Term-Project/src/Util/Student.java
@@ -4,13 +4,43 @@ package Util;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * A class to store information about students.
+ * @author Branden Wong - 30040675
+ * @author Savipal Jessel - 30039257
+ * @version 2.0
+ */
 public class Student implements Serializable {
+	/**
+	 * the serial ID of the class
+	 */
 	private static final long serialVersionUID=2L;
+
+	/**
+	 * the name of the student
+	 */
 	private String studentName;
+
+	/**
+	 * the student ID for the student.
+	 */
 	private int studentId;
+
+	/**
+	 * The offerings that the student is enrolled in
+	 */
 	private ArrayList<CourseOffering> offeringList;
+
+	/**
+	 * All of the course registrations that the student had.
+	 */
 	private ArrayList<Registration> studentRegList;
 
+	/**
+	 * A constructor for the student
+	 * @param studentName the student's name
+	 * @param studentId the student's ID
+	 */
 	public Student (String studentName, int studentId) {
 		this.setStudentName(studentName);
 		this.setStudentId(studentId);
@@ -18,21 +48,42 @@ public class Student implements Serializable {
 		offeringList = new ArrayList<CourseOffering>();
 	}
 
+	/**
+	 * gets the student's name.
+	 * @return the name of the student
+	 */
 	public String getStudentName() {
 		return studentName;
 	}
 
+	/**
+	 * Sets the student's name
+	 * @param studentName the name to set.
+	 */
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
 
+	/**
+	 * Gets the student's ID
+	 * @return the students ID
+	 */
 	public int getStudentId() {
 		return studentId;
 	}
 
+	/**
+	 * Sets the student's ID
+	 * @param studentId the ID to set the student to.
+	 */
 	public void setStudentId(int studentId) {
 		this.studentId = studentId;
 	}
+
+	/**
+	 * Adds the student to a course offering.
+	 * @param offering the offering to add the student to.
+	 */
 	public void addOffering(CourseOffering offering){
 		if(offeringList.size() < 6){
 			offeringList.add(offering);
@@ -41,6 +92,11 @@ public class Student implements Serializable {
 			System.out.println("Student is already enrolled in 6 courses. Cannot enroll in any more this semester.");
 		}
 	}
+
+	/**
+	 * Gets information about the student.
+	 * @return the information about the student in a string form.
+	 */
 	@Override
 	public String toString () {
 		String st = "Student Name: " + getStudentName() + "\n" +
@@ -48,6 +104,10 @@ public class Student implements Serializable {
 		return st;
 	}
 
+	/**
+	 * Prints the registration list in a string.
+	 * @return the registration list in string form.
+	 */
 	public String registrationListToString(){
 		String st = "";
 		for(int i = 0; i < studentRegList.size(); ++i){
@@ -56,12 +116,23 @@ public class Student implements Serializable {
 		}
 		return st;
 	}
+
+	/**
+	 * Adds a registration to the student
+	 * @param registration the registration to add.
+	 */
 	public void addRegistration(Registration registration) {
 		// TODO Auto-generated method stub
 		studentRegList.add(registration);
 
 	}
 
+	/**
+	 * checks if the student is in the specified course
+	 * @param courseName the name of the course
+	 * @param courseNum the course number
+	 * @return true if the student is enrolled in this course
+	 */
 	public boolean isInCourse(String courseName, int courseNum){
 		for(int i = 0; i < offeringList.size(); ++i){
 			if(offeringList.get(i).isCourse(courseName, courseNum)){
@@ -71,6 +142,11 @@ public class Student implements Serializable {
 		return false;
 	}
 
+	/**
+	 * Removes the student from the course
+	 * @param courseName the name of the course
+	 * @param courseNum the course number
+	 */
 	public void removeFrom(String courseName, int courseNum) {
 		for(int i = 0; i < offeringList.size(); ++i){
 			if(offeringList.get(i).isCourse(courseName, courseNum)){
