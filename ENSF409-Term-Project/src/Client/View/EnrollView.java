b02379cb90c572b00c437db894c06b7f3e9b661f
Savi Jessel <savipal.jessel@gmail.com>
2020-04-13 17:41:17 -0600

    added javadocs

--- ENSF409-Term-Project/src/Client/View/EnrollView.java
+++ ENSF409-Term-Project/src/Client/View/EnrollView.java
@@ -5,25 +5,98 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * Secondary view class that creates the menu for the Enroll feature and provides methods for access and dialog.
+ *
+ * @author Branden Wong - 30040675
+ * @author Savipal Jessel - 30039257
+ * @version 1.0
+ */
+
 public class EnrollView extends JFrame {
+    /**
+     * The label for the title for the enroll view
+     */
     private JLabel title;
+
+    /**
+     * The label for the name of the course
+     */
     private JLabel courseName;
+
+    /**
+     * The label for the course number
+     */
     private JLabel courseNum;
+
+    /**
+     * The label for the name of the student
+     */
     private JLabel studentName;
+
+    /**
+     * The label for the student number
+     */
     private JLabel studentNum;
+
+    /**
+     * The label for the section number
+     */
     private JLabel sectionNum;
+
+    /**
+     * The enroll button
+     */
     private JButton enroll;
+
+    /**
+     * The cancel button
+     */
     private JButton cancel;
+
+    /**
+     * The text field for the name of the course
+     */
     private JTextField courseNameField;
+
+    /**
+     * The text field for the course number
+     */
     private JTextField courseNumField;
+
+    /**
+     * The text field for the name of the student
+     */
     private JTextField studentNameField;
+
+    /**
+     * The text field for the student number
+     */
     private JTextField studentNumField;
+
+    /**
+     * The text field for the section number
+     */
     private JTextField sectionNumField;
+
+    /**
+     * The north side panel
+     */
     private JPanel northPanel;
+
+    /**
+     * The center panel
+     */
     private JPanel centerPanel;
+
+    /**
+     * The south side panel
+     */
     private JPanel southPanel;
 
-
+    /**
+     * Sets-up the frame, populates the panels, and adds the frames to the panel for the enroll view
+     */
     public EnrollView(){
         super();
         setSize(250,150);
@@ -70,6 +143,9 @@ public class EnrollView extends JFrame {
         getContentPane().add(BorderLayout.SOUTH, southPanel);
     }
 
+    /**
+     * Clears all of the text fields
+     */
     public void clearTextFields(){
         studentNameField.setText("");
         studentNumField.setText("");
@@ -78,6 +154,10 @@ public class EnrollView extends JFrame {
         sectionNumField.setText("");
     }
 
+    /**
+     * Gets a list of the contents for all of the text fields
+     * @return a list containing the contents of each text field
+     */
     public ArrayList<String> getFields(){
         ArrayList<String> courseInfo = new ArrayList<>();
         courseInfo.add(studentNameField.getText());
@@ -96,10 +176,17 @@ public class EnrollView extends JFrame {
         JOptionPane.showMessageDialog(this, message);
     }
 
+    /**
+     * Adds an Action Listener for the enroll button
+     * @param a the Action Listener
+     */
     public void addEnrollListener(ActionListener a){
         enroll.addActionListener(a);
     }
-
+    /**
+     * Adds an Action Listener for the cancel button
+     * @param a the Action Listener
+     */
     public void addCancelListener(ActionListener a){
         cancel.addActionListener(a);
     }
--- ENSF409-Term-Project/src/Client/View/RemoveView.java
+++ ENSF409-Term-Project/src/Client/View/RemoveView.java
@@ -5,23 +5,88 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * Secondary view class that creates the menu for the Remove feature and provides methods for access and dialog.
+ *
+ * @author Branden Wong - 30040675
+ * @author Savipal Jessel - 30039257
+ * @version 1.0
+ */
+
 public class RemoveView extends JFrame {
+    /**
+     * The label for the title for the enroll view
+     */
     private JLabel title;
+
+    /**
+     * The label for the name of the course
+     */
     private JLabel courseName;
+
+    /**
+     * The label for the course number
+     */
     private JLabel courseNum;
+
+    /**
+     * The label for the name of the student
+     */
     private JLabel studentName;
+
+    /**
+     * The label for the student number
+     */
     private JLabel studentNum;
+
+    /**
+     * The remove button
+     */
     private JButton remove;
+
+    /**
+     * The cancel button
+     */
     private JButton cancel;
+    /**
+     * The text field for the name of the course
+     */
     private JTextField courseNameField;
+
+    /**
+     * The text field for the course number
+     */
     private JTextField courseNumField;
+
+    /**
+     * The text field for the name of the student
+     */
     private JTextField studentNameField;
+
+    /**
+     * The text field for the student number
+     */
     private JTextField studentNumField;
+
+    /**
+     * The north side panel
+     */
     private JPanel northPanel;
+
+    /**
+     * The center panel
+     */
     private JPanel centerPanel;
+
+    /**
+     * The south side panel
+     */
     private JPanel southPanel;
 
 
+    /**
+     * Sets-up the frame, populates the panels, and adds the frames to the panel for the remove view
+     */
     public RemoveView(){
         super();
         setSize(250,150);
@@ -64,6 +129,9 @@ public class RemoveView extends JFrame {
         getContentPane().add(BorderLayout.SOUTH, southPanel);
     }
 
+    /**
+     * Clears all of the text fields
+     */
     public void clearTextFields(){
         studentNameField.setText("");
         studentNumField.setText("");
@@ -71,6 +139,10 @@ public class RemoveView extends JFrame {
         courseNumField.setText("");
     }
 
+    /**
+     * Gets a list of the contents for all of the text fields
+     * @return a list containing the contents of each text field
+     */
     public ArrayList<String> getFields(){
         ArrayList<String> courseInfo = new ArrayList<>();
         courseInfo.add(studentNameField.getText());
@@ -88,10 +160,18 @@ public class RemoveView extends JFrame {
         JOptionPane.showMessageDialog(this, message);
     }
 
+    /**
+     * Adds an Action Listener for the remove button
+     * @param a the Action Listener
+     */
     public void addRemoveListener(ActionListener a){
         remove.addActionListener(a);
     }
 
+    /**
+     * Adds an Action Listener for the cancel button
+     * @param a the Action Listener
+     */
     public void addCancelListener(ActionListener a){
         cancel.addActionListener(a);
     }
