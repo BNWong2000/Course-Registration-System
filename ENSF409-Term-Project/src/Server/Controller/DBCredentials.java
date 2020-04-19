2c3d77f563b61af9b00bc6cd615e6d92133e5083
Savi Jessel <savipal.jessel@gmail.com>
2020-04-19 15:33:29 -0600

    small changes

--- ENSF409-Term-Project/src/Server/Controller/DBCredentials.java
+++ ENSF409-Term-Project/src/Server/Controller/DBCredentials.java
@@ -2,11 +2,17 @@ package Server.Controller;
 
 /**
  * An interface to manage the JDBC Driver, URL, and username/password for the mySQL connection
+ *
+ * @author Savipal Jessel
+ * @author Branden Wonh
+ * @version 1.0
+ *
  */
 public interface DBCredentials {
 	
-	// JDBC driver name and database URL
-	   static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+		// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
 	   static final String DB_URL = "jdbc:mysql://localhost:3306/CourseRegistration";
 
 	   //  Database credentials
